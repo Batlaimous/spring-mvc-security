c738b73dffb0bdbc7aa280292b560f51c3d83d6e
beshoy_younan <beshoy.younan@outlook.com>
2024-10-23 15:59:46 -0300

    Initial commit

---
+++ src/test/java/com/besho/springboot/mvcsecuritydemo/MvcsecuritydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.besho.springboot.mvcsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcsecuritydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
